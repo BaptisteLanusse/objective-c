6ca870fc649b3ee383a0249c82a5e00bf2081dc7
BaptisteLanusse <baptiste.lanusse@y-nov.com>
2015-11-02 11:37:16 +0100

    ok

---
+++ informations-tabulaires/informations-tabulaires/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  informations-tabulaires
+//
+//  Created by Lanusse Baptiste on 02/11/2015.
+//  Copyright © 2015 Baptiste Lanusse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ informations-tabulaires/informations-tabulaires/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  informations-tabulaires
+//
+//  Created by Lanusse Baptiste on 02/11/2015.
+//  Copyright © 2015 Baptiste Lanusse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
